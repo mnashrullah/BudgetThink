1b52024eb4f43ffff8aaf828d75509898a58d56f
Muhammad Naratama <mnaratama@gmail.com>
2020-05-26 05:10:03 +0700

    Nambah toolbar Date picker, description

--- BudgetThink/InputDataController.swift
+++ BudgetThink/InputDataController.swift
@@ -7,9 +7,16 @@
 //
 import UIKit
 
-class InputDataController: UIViewController {
+class InputDataController: UIViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
 
     var isActive:Bool = false
+    
+    @IBOutlet weak var PickADateTextField: UITextField!
+    @IBOutlet weak var DescriptionTextField: UITextField!
+    @IBOutlet weak var TotalValueTextField: UITextField!
+    @IBOutlet weak var DatePicker: UIView!
+    
+    let datePickerToolbar = UIDatePicker()
     
     
     @IBOutlet weak var ExpenseButton: UIButton!
@@ -42,9 +49,13 @@ class InputDataController: UIViewController {
         }
         
     @IBOutlet weak var WhiteView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         SwitchButton.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+        
+        
         let newLayer = CAGradientLayer()
         newLayer.colors = [UIColor.darkBlueGradient.cgColor, UIColor.lightBlueGradient.cgColor]
         newLayer.frame = view.frame
@@ -52,8 +63,77 @@ class InputDataController: UIViewController {
         newLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
         view.layer.insertSublayer(newLayer, at: 0)
         
-       WhiteView.layer.cornerRadius = 45
+        WhiteView.layer.cornerRadius = 45
+        
+        PickADateTextField.text = "Pick a Date"
+        PickADateTextField.textColor = UIColor.blackText
+        
+        DescriptionTextField.text = "Description"
+        DescriptionTextField.textColor = UIColor.lightGreyDesc
+        
+        DatePicker.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.viewTapped(sender:)))
+        tap.delegate = self
+        DatePicker.addGestureRecognizer(tap)
+        
+        createDatePicker()
     }
 
-
+    func createDatePicker() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        
+        PickADateTextField.inputAccessoryView = toolbar
+        PickADateTextField.inputView = datePickerToolbar
+        datePickerToolbar.datePickerMode = .date
+        
+    }
+    
+    @objc func donePressed(){
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        PickADateTextField.text = formatter.string(from: datePickerToolbar.date)
+        self.view.endEditing(true)
+    }
+    
+    
+    @objc func viewTapped(sender: UITapGestureRecognizer){
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        
+        toolbar.setItems([doneBtn], animated: true)
+        
+        PickADateTextField.inputAccessoryView = toolbar
+        PickADateTextField.inputView = datePickerToolbar
+        datePickerToolbar.datePickerMode = .date
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    @IBAction func DescEditBegin(_ sender: UITextField) {
+        if DescriptionTextField.textColor == UIColor.lightGreyDesc {
+            DescriptionTextField.text = nil
+            DescriptionTextField.textColor = UIColor.blackText
+        }
+    }
+    
+    @IBAction func DescEditEnd(_ sender: UITextField) {
+        if DescriptionTextField.text!.isEmpty {
+            DescriptionTextField.text = "Description"
+            DescriptionTextField.textColor = UIColor.lightGreyDesc
+        }
+    }
+    
 }
