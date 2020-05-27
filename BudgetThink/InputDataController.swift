892f6fa72de7356d0cbe9e27b5f60cfb649dd613
farrasdoko <farras.abiyyu.doko@gmail.com>
2020-05-27 17:13:25 +0700

    - fix storyboard for iPhone SE 2020 - add repeat clicked - add receipt clicked

--- BudgetThink/InputDataController.swift
+++ BudgetThink/InputDataController.swift
@@ -15,6 +15,9 @@ class InputDataController: UIViewController, UITextFieldDelegate, UIGestureRecog
     @IBOutlet weak var DescriptionTextField: UITextField!
     @IBOutlet weak var TotalValueTextField: UITextField!
     @IBOutlet weak var DatePicker: UIView!
+    @IBOutlet weak var ReceiptView: UIView!
+    @IBOutlet weak var ReceiptImage: UIImageView!
+    @IBOutlet weak var RepeatView: UIView!
     
     let datePickerToolbar = UIDatePicker()
     
@@ -47,7 +50,10 @@ class InputDataController: UIViewController, UITextFieldDelegate, UIGestureRecog
             
             
         }
-        
+    @IBAction func backTap(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     @IBOutlet weak var WhiteView: UIView!
     
     
@@ -68,8 +74,8 @@ class InputDataController: UIViewController, UITextFieldDelegate, UIGestureRecog
         PickADateTextField.text = "Pick a Date"
         PickADateTextField.textColor = UIColor.blackText
         
-        DescriptionTextField.text = "Description"
-        DescriptionTextField.textColor = UIColor.lightGreyDesc
+//        DescriptionTextField.text = "Description"
+//        DescriptionTextField.textColor = UIColor.lightGreyDesc
         
         DatePicker.isUserInteractionEnabled = true
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.viewTapped(sender:)))
@@ -77,6 +83,52 @@ class InputDataController: UIViewController, UITextFieldDelegate, UIGestureRecog
         DatePicker.addGestureRecognizer(tap)
         
         createDatePicker()
+        
+        ReceiptView.isUserInteractionEnabled = true
+        let imagePicker = UITapGestureRecognizer(target: self, action: #selector(pickImage))
+        ReceiptView.addGestureRecognizer(imagePicker)
+        
+        RepeatView.isUserInteractionEnabled = true
+        let showRepeatModal = UITapGestureRecognizer(target: self, action: #selector(repeatTapped))
+        RepeatView.addGestureRecognizer(showRepeatModal)
+    }
+    
+    @objc func pickImage() {
+        
+        let prompt = UIAlertController(title: "Choose a Photo",
+                                       message: "Please choose a photo.",
+                                       preferredStyle: .actionSheet)
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        func presentCamera(_ _: UIAlertAction) {
+            imagePicker.sourceType = .camera
+            self.present(imagePicker, animated: true)
+        }
+        
+        let cameraAction = UIAlertAction(title: "Camera",
+                                         style: .default,
+                                         handler: presentCamera)
+        
+        func presentAlbums(_ _: UIAlertAction) {
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true)
+        }
+        
+        let albumsAction = UIAlertAction(title: "Photo Library",
+                                         style: .default,
+                                         handler: presentAlbums)
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel,
+                                         handler: nil)
+        
+        prompt.addAction(cameraAction)
+        prompt.addAction(albumsAction)
+        prompt.addAction(cancelAction)
+        
+        self.present(prompt, animated: true, completion: nil)
     }
 
     func createDatePicker() {
@@ -115,6 +167,14 @@ class InputDataController: UIViewController, UITextFieldDelegate, UIGestureRecog
         datePickerToolbar.datePickerMode = .date
     }
     
+    @objc func repeatTapped() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let repeatVC = storyboard.instantiateViewController(identifier: "RepeatVC") as RepeatViewController
+        
+        repeatVC.modalPresentationStyle = .custom
+        self.present(repeatVC, animated: true)
+    }
+    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -122,17 +182,35 @@ class InputDataController: UIViewController, UITextFieldDelegate, UIGestureRecog
         return true
     }
     
-    @IBAction func DescEditBegin(_ sender: UITextField) {
-        if DescriptionTextField.textColor == UIColor.lightGreyDesc {
-            DescriptionTextField.text = nil
-            DescriptionTextField.textColor = UIColor.blackText
-        }
+}
+
+extension InputDataController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss picker, returning to original root viewController.
+        dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func DescEditEnd(_ sender: UITextField) {
-        if DescriptionTextField.text!.isEmpty {
-            DescriptionTextField.text = "Description"
-            DescriptionTextField.textColor = UIColor.lightGreyDesc
+    internal func imagePickerController(_ picker: UIImagePickerController,
+                                        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        // Extract chosen image.
+        let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        // Display image on screen.
+        show(originalImage)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func show(_ image: UIImage) {
+        ReceiptImage.image = nil
+        ReceiptImage.image = image
+        
+        // Transform image to CGImage.
+        guard let cgImage = ReceiptImage.image?.cgImage else {
+            print("Trying to show an image not backed by CGImage!")
+            return
         }
     }
     
---
+++ BudgetThink/RepeatViewController.swift
@@ -0,0 +1,71 @@
+//
+//  RepeatViewController.swift
+//  BudgetThink
+//
+//  Created by Farras Doko on 26/05/20.
+//  Copyright © 2020 Muhammad Nashrullah. All rights reserved.
+//
+
+import UIKit
+
+class RepeatViewController: UIViewController {
+    
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var backdropView: UIView!
+    @IBOutlet weak var periodView: UIView!
+    @IBOutlet weak var periodTxt: UILabel!
+    @IBOutlet weak var amountTxt: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modalPresentationStyle = .custom
+        transitioningDelegate = self
+        view.backgroundColor = .clear
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backdropTap(_:)))
+        backdropView.addGestureRecognizer(tapGesture)
+        
+        let periodGesture = UITapGestureRecognizer(target: self, action: #selector(periodClick))
+        periodView.addGestureRecognizer(periodGesture)
+    }
+    
+    @objc func periodClick() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        func alertHandler(action: UIAlertAction) {
+            self.periodTxt.text = action.title
+        }
+        
+        let daily = UIAlertAction(title: "Daily", style: .default, handler: alertHandler)
+        let weekly = UIAlertAction(title: "Weekly", style: .default, handler: alertHandler)
+        let monthly = UIAlertAction(title: "Monthly", style: .default, handler: alertHandler)
+        let annual = UIAlertAction(title: "Annual", style: .default, handler: alertHandler)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        })
+        for action in [daily, weekly, monthly, annual, cancel] {
+            alert.addAction(action)
+        }
+        
+        self.present(alert, animated: true)
+        
+    }
+    
+    @objc func backdropTap(_ sender: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func stepperClick(_ sender: UIStepper) {
+        sender.minimumValue = 0
+        sender.stepValue = 1
+        amountTxt.text = String(sender.value)
+    }
+    
+}
+
+extension RepeatViewController: UIViewControllerTransitioningDelegate  {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 1
+    }
+    
+}
